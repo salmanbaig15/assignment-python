ea8d254a26c9444b7470000fe1f25195af9745a7
Salman <113794549+salmanbaig15@users.noreply.github.com>
2022-09-18 15:12:27 +0530

    assignment 1

---
+++ Desktop/Python Assignments/assignment1.py
@@ -0,0 +1,24 @@
+#1
+from platform import python_version
+print(python_version())
+#2
+print("Hello Python!")
+#3
+print("Hello Python!")
+#4
+print("Hello Python!")
+#5
+print("Hello Python", sep="\n")
+#6
+print("\"MySirG\"")
+#7
+print("\"Teacher's Day\"")
+#8
+a,b = "Salman","Baig"
+print(a,b)
+#9
+a,b,c,d = "Salman", 29, "PG Diploma", "7"
+print("{} is {} years old with {} degree having {} years of total experience".format(a,b,c,d))
+#10
+a,b,c = 1,2,3
+print(a,b,c, sep="\n")
