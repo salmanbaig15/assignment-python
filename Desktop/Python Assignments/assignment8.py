2ff1b8698b7b577440478c335321aa3c2e50fc81
Salman <113794549+salmanbaig15@users.noreply.github.com>
2022-09-18 23:35:22 +0530

    assignment 8

---
+++ Desktop/Python Assignments/assignment8.py
@@ -0,0 +1,71 @@
+#1
+i = 0
+while i<5:
+    print("MySirG")
+    i+=1
+#2
+i = 1
+while i<=10:
+    print(i, end=' ')
+    i+=1
+print()
+#3
+i = 10
+while i>0:
+    print(i, end=' ')
+    i-=1
+print()
+#4
+i = 1
+cnt = 10
+while cnt>0:
+    if i%2!=0:
+        print(i, end=' ')
+        cnt-=1
+    i+=1
+print()
+#5
+i = 20
+cnt = 1
+while cnt<=10:
+    if i%2!=0:
+        print(i, end=' ')
+        cnt+=1
+    i-=1
+print()
+#6
+i = 1
+cnt = 10
+while cnt>0:
+    if i%2==0:
+        print(i, end=' ')
+        cnt-=1
+    i+=1
+print()
+#7
+i = 20
+cnt = 1
+while cnt<=10:
+    if i%2==0:
+        print(i, end=' ')
+        cnt+=1
+    i-=1
+print()
+#8
+i=1
+while i<=10:
+    print(i**2, end=' ')
+    i+=1
+print()
+#9
+i=1
+while i<=10:
+    print(i**3, end=' ')
+    i+=1
+print()
+#10
+i=1
+while i<=10:
+    print(5*i, end=' ')
+    i+=1
+print()
