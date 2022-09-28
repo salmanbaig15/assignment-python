1ebae94bbd0cd958cebc250c2b0c38c969b9d1a0
Salman <113794549+salmanbaig15@users.noreply.github.com>
2022-09-29 01:43:29 +0530

    assignment 15

---
+++ Desktop/Python Assignments/assignment15.py
@@ -0,0 +1,32 @@
+#1
+s = 'Salman'
+s1 = str('Baig')
+print(s,s1)
+#2
+s = 'iNeuron'
+print(s[:5])
+#3
+s = 'Hello Learners'
+print(s[2:5])
+#4
+a = 'Learning'
+b = 'Python'
+print(a+' '+b)
+#5
+a = 'iNeuron'
+print(len(a))
+#6
+print(a[::-1])
+#7
+s = 'Hello World'
+s1 = 'Hello'
+print('Do we have {} in {}: {}'.format(s1, s, s1 in s))
+#8
+s = '12345'
+print(s.isnumeric())
+#9
+s = 'abc'
+print(s.isalpha())
+#10
+i = 10
+print(str(i), type(str(i)))
